51c4865282c911b09ce584c986d6ad2d06aedd07
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-05-15 21:30:38 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -5,19 +5,17 @@ import pyscroll
 class Inventory(pygame.sprite.Sprite):
     def __init__(self, screen):
         print('INVENTORY LOAD')
-        self.player = 'yaya'
         self.inv = []
         self.inventory_img = 'img/icon.png'
         self.screen = screen
 
-    def open_inventory(self):
-        print('Open Inventory is ' + str(self.inv))
-
-
-
-    def close_inventory(self):
-        print('Close Inventory for ' + str(self.player))
-
+    def draw_inventory(self):
+        menu = pygame.image.load("img/menu.jpg").convert_alpha()
+        menu = pygame.transform.scale(menu, self.screen.get_size())
+        if self.inventory_is_open:
+            self.screen.blit(menu, (0, 0))
+        else:
+            menu.fill((0, 0, 0))
 
 
     def add_inventory(self, item):
@@ -25,3 +23,15 @@ class Inventory(pygame.sprite.Sprite):
             self.inv.append(item)
         else:
             print("No inventory space")
+
+    def run(self):
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_e]:
+            #self.open_inventory()
+            self.inventory_is_open = True
+            self.draw_inventory()
+        else:
+            #self.close_inventory()
+            self.inventory_is_open = False
+            self.draw_inventory()
