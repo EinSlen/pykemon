9c6db9236f3b23da126269c77ecc090051e42d59
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-05-16 23:32:58 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,13 +1,16 @@
 import pygame
+from function import Function
 
 class Inventory(pygame.sprite.Sprite):
-    def __init__(self, screen, group):
+    def __init__(self, screen, group, tmx_data):
         print('INVENTORY LOAD')
         self.inv = []
         self.menu = pygame.image.load("img/menu.jpg").convert_alpha()
         self.screen = screen
         self.group = group
         self.inventory_is_open = False
+        self.function = Function(self.screen)
+        self.tmx_data = tmx_data
 
     def draw_inventory(self):
         if self.inventory_is_open:
@@ -16,30 +19,30 @@ class Inventory(pygame.sprite.Sprite):
             for sprite in self.group.sprites():
                 sprite.move_back()
             for i in self.inv:
-                print('Inventory' + i)
-    def redraw_inventory(self):
-        if self.inventory_is_open == False:
-            self.menu.fill((0, 0, 0))
+                print('Inventory : ' + i)
 
-    def add_inventory(self, item):
+    def add_inventory(self):
         if len(self.inv) < 6:
-            self.inv.append(item)
+            self.inv.append('Add')
         else:
-            print("No inventory space")
+            self.function.error('No add inventory')
 
     def remove_inventory(self, item):
         if len(self.inv) <= 1:
             self.inv.remove(item)
         else:
-            print('No remove from inventory')
+            self.function.error('No remove inventory')
 
-    def run(self):
+    def run(self, tmx_data, map):
+        self.tmx_data = tmx_data
+        self.map = map
         pressed = pygame.key.get_pressed()
 
         self.draw_inventory()
-
         if pressed[pygame.K_e]:
-            #self.open_inventory()
             self.inventory_is_open = True
         elif pressed[pygame.K_ESCAPE]:
             self.inventory_is_open = False
+        elif pressed[pygame.K_f]:
+            self.add_inventory()
+
