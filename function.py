9287a1f6a38862c031b1eddec5d53c2ad9b66d1b
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-05-16 23:33:40 +0200

    Create function.py

---
+++ function.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageDraw
+import pygame
+
+class Function:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def error(self, msg):
+        img = Image.new('RGB', (130, 30), color=(0, 0, 0))
+        d = ImageDraw.Draw(img)
+        d.text((10, 10), msg, fill=(255, 0, 0))
+        img.save('img/error.png')
+
+        draw_image = pygame.image.load("img/error.png").convert_alpha()
+        self.screen.blit(draw_image, (0, 0))
+
+
+
+
