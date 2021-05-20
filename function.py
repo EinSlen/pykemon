9288d6ca7633aa344870b2a75b25700f8283ea1f
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-05-20 11:31:25 +0200

    Update function.py

--- function.py
+++ function.py
@@ -14,6 +14,15 @@ class Function:
         draw_image = pygame.image.load("img/error.png").convert_alpha()
         self.screen.blit(draw_image, (0, 0))
 
+    def information(self, msg):
+        img = Image.new('RGB', (150, 30), color=(0, 0, 0))
+        d = ImageDraw.Draw(img)
+        d.text((10, 10), msg, fill=(255, 165, 0))
+        img.save('img/info.png')
+
+        draw_image = pygame.image.load('img/info.png').convert_alpha()
+        self.screen.blit(draw_image, (650, 1))
+
 
 
 
