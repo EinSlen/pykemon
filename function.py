4bd1c0737d4c095cd141d10546ebe14bc53efb3c
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-05-23 13:55:11 +0200

    add create function

--- function.py
+++ function.py
@@ -6,7 +6,7 @@ class Function:
         self.screen = screen
 
     def error(self, msg):
-        img = Image.new('RGB', (130, 30), color=(0, 0, 0))
+        img = Image.new('RGB', (200, 30), color=(0, 0, 0))
         d = ImageDraw.Draw(img)
         d.text((10, 10), msg, fill=(255, 0, 0))
         img.save('img/error.png')
@@ -22,6 +22,15 @@ class Function:
 
         draw_image = pygame.image.load('img/info.png').convert_alpha()
         self.screen.blit(draw_image, (650, 1))
+
+    def create(self, msg):
+        img = Image.new('RGB', (80, 30), color=(255, 255, 255))
+        d = ImageDraw.Draw(img)
+        d.text((25, 10), msg, fill=(255, 50, 0))
+        img.save('img/' + msg + '.png')
+
+        draw = pygame.image.load('img/' + msg + '.png').convert_alpha()
+        self.screen.blit(draw, (0, 0))
 
 
 
